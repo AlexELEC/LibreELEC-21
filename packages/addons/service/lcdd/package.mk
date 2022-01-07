5ebd83a3f3b58eba6f355ed284123bd4025f935a
heitbaum <rudi@heitbaum.com>
2022-01-07 10:08:55 +0000

    lcdd: update to githash 71877ee and addon (1)
    fixes:
    - https://github.com/lcdproc/lcdproc/commit/71877ee059a238400b5f6b41cee7c43a3df00334
    - hd44780/gpiod: rewrite to support libgpiod2
    - https://github.com/lcdproc/lcdproc as 214

--- packages/addons/service/lcdd/package.mk
+++ packages/addons/service/lcdd/package.mk
@@ -2,15 +2,15 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="lcdd"
-PKG_VERSION="9ec9ba4e5dda653288bc55d2898723aa2c2ad9c1"
-PKG_SHA256="442f60fc7c26847508e7fb99d901e905016c136d0f6eb320e3262bef20f39452"
-PKG_VERSION_DATE="0.5dev+2020-07-21"
-PKG_REV="0"
+PKG_VERSION="71877ee059a238400b5f6b41cee7c43a3df00334"
+PKG_SHA256="e910c7d748b58cc57543dc8845f8b2bcd0592dddd58bd72a55a58e90e6bfd861"
+PKG_VERSION_DATE="0.5dev+2024-12-14"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://lcdproc.org/"
 PKG_URL="https://github.com/lcdproc/lcdproc/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain freetype libftdi1 libhid libugpio libusb ncurses serdisplib"
+PKG_DEPENDS_TARGET="toolchain freetype libftdi1 libgpiod libhid libugpio libusb ncurses serdisplib"
 PKG_SECTION="service"
 PKG_SHORTDESC="LCDproc: Software to display system information from your Linux/*BSD box on a LCD"
 PKG_LONGDESC="LCDproc (${PKG_VERSION}) is a piece of software that displays real-time system information from your Linux/*BSD box on a LCD. The server supports several serial devices: Matrix Orbital, Crystal Fontz, Bayrad, LB216, LCDM001 (kernelconcepts.de), Wirz-SLI, Cwlinux(.com) and PIC-an-LCD; and some devices connected to the LPT port: HD44780, STV5730, T6963, SED1520 and SED1330. Various clients are available that display things like CPU load, system load, memory usage, uptime, and a lot more."
@@ -21,13 +21,13 @@ PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="LCDproc"
 PKG_ADDON_TYPE="xbmc.service"
 
-PKG_CONFIGURE_OPTS_TARGET="--with-ft-prefix=${SYSROOT_PREFIX}/usr \
-                           --enable-libusb \
-                           --enable-libftdi \
-                           --disable-libX11 \
+PKG_CONFIGURE_OPTS_TARGET="--enable-drivers=all \
+                           --enable-freetype \
                            --enable-libhid \
+                           --enable-libftdi \
                            --disable-libpng \
-                           --enable-drivers=all"
+                           --enable-libusb \
+                           --disable-libX11"
 
 pre_configure_target() {
   CFLAGS+=" -O3"
