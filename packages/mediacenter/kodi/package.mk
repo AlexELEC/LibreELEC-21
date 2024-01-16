4c679c1d8c87b13ead652442a08da9d4624e41a4
AlexELEC <alexelecv5@gmail.com>
2024-01-16 08:35:04 +0000

    kodi: bump to 78a2ef1

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="f3f1df1eab2a38b7039e57635a6597b37510481a"
-PKG_SHA256="dfc46bc48a2d226bd121828b2370ea7be8ed38fc2a8dcd2cf2ed70fc4582a84a"
+PKG_VERSION="78a2ef1"
+PKG_SHA256="1292c081d5779ecf39b254da07127e6de4d5c750d313d9a852b2d5e2fef95228"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/xbmc/xbmc/archive/${PKG_VERSION}.tar.gz"
