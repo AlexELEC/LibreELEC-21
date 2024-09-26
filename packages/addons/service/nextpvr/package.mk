282b913aefed4599ada598991eae323412373b66
Martin Vallevand <mvallevand@gmail.com>
2024-09-26 10:36:41 -0400

    [le12] service.nextpvr backport of PR 9323
    Also include PR #9507
    
    Please do not merge maintenance release no yet on GitHub

--- packages/addons/service/nextpvr/package.mk
+++ packages/addons/service/nextpvr/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nextpvr"
-PKG_VERSION="6.1.5~Omega"
-PKG_ADDON_VERSION="6.1.5~4"
+PKG_VERSION="7.0.1~Omega"
+PKG_ADDON_VERSION="7.0.1~2"
 PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="NextPVR"
 PKG_SITE="https://nextpvr.com"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain libhdhomerun libmediainfo comskip"
 PKG_SECTION="service"
 PKG_SHORTDESC="NextPVR Server"
 PKG_LONGDESC="NextPVR is a personal video recorder application. It allows to watch or record live TV, provides great features like series recordings and web scheduling."
@@ -25,4 +25,13 @@ addon() {
 
 post_install_addon() {
   sed -e "s/@NEXTPVR_VERSION@/${PKG_ADDON_VERSION}/g" -i "${INSTALL}/bin/nextpvr-downloader"
+
+  mkdir -p ${INSTALL}/{lbin,lib.private}
+  cp $(get_build_dir libmediainfo)/Project/GNU/Library/.libs/libmediainfo.so ${INSTALL}/lib.private
+  cp -P $(get_build_dir libhdhomerun)/hdhomerun_config ${INSTALL}/lbin
+  cp -P $(get_install_dir comskip)/usr/bin/comskip ${INSTALL}/lbin
+  if [ "${TARGET_ARCH}" = "x86_64" ]; then
+    cp -P $(get_install_dir x265)/usr/lib/libx265.so.212 ${INSTALL}/lib.private
+    patchelf --add-rpath '${ORIGIN}/../lib.private' ${INSTALL}/lbin/comskip
+  fi
 }
