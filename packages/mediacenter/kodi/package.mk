a8ceea6948cf4b4381ba22ea54e0270f694622bc
AlexELEC <alexelecv5@gmail.com>
2024-10-14 14:31:55 +0000

    kodi: bump to 702319a

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="21.1-Omega"
-PKG_SHA256="ad6e40b0912c8318635f0501dc1f7f27ce3a29e671f2ddb608cad34babae80ef"
+PKG_VERSION="702319a"
+PKG_SHA256="d4d3d89b5588e7c0f6adbcfdfa96852f5d4a7e95d972ba98e566b5b2a69aeb52"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/xbmc/xbmc/archive/${PKG_VERSION}.tar.gz"
