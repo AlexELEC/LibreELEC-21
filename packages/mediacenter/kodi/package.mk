5033eb68bbc4fe0cf568f17ba7a6e0dba0b421a9
Matthias Reichl <hias@horus.com>
2024-11-01 10:29:39 +0100

    kodi: update to 2b2d09e
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="21.1-Omega"
-PKG_SHA256="ad6e40b0912c8318635f0501dc1f7f27ce3a29e671f2ddb608cad34babae80ef"
+PKG_VERSION="2b2d09e2902fa6cff8ceb2da99c9fca87743a509"
+PKG_SHA256="59330b8117906ab3d7c78cb8d827febbcad74732850fe0153f0b848f70fcd56b"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/xbmc/xbmc/archive/${PKG_VERSION}.tar.gz"
