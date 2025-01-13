657f57efe87bf320b8a5383e753a7bd4f175d0e2
Matthias Reichl <hias@horus.com>
2025-01-13 12:37:40 +0100

    dtv-scan-tables: update to 2024-03-24-7098bdd27548
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/addons/addon-depends/dtv-scan-tables/package.mk
+++ packages/addons/addon-depends/dtv-scan-tables/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dtv-scan-tables"
-PKG_VERSION="2022-04-30-57ed29822750"
-PKG_SHA256="6a6268aa392459378fa3a13922fc015a3fa63ff822f4b0d64d33d71350a6ec9e"
+PKG_VERSION="2024-03-24-7098bdd27548"
+PKG_SHA256="3f4c8981b3a4829066b396abc0da270e5c7e674b5d190e92532a06951a07e98e"
 PKG_LICENSE="GPL"
 PKG_SITE="https://git.linuxtv.org/dtv-scan-tables.git"
 PKG_URL="https://linuxtv.org/downloads/dtv-scan-tables/dtv-scan-tables-${PKG_VERSION}.tar.bz2"
