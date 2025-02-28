96d64db03348647d89d337ae7b90fa0c4d633c08
AlexELEC <alexelecv5@gmail.com>
2025-02-28 06:34:38 +0000

    kodi: bump to b951fb7

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="21.2-Omega"
-PKG_SHA256="da3a5df663684664b9383b65f1c06568222629d935084a59e4e641fcdcb6c383"
+PKG_VERSION="b951fb7"
+PKG_SHA256="6a30afcaf4d8e01bdade57d239c255565d7e56083880031e21ad256dfc01d640"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/xbmc/xbmc/archive/${PKG_VERSION}.tar.gz"
