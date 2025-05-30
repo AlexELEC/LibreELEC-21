b733be15a185fd8341b7294ebd8722738c82d085
AlexELEC <alexelecv5@gmail.com>
2025-05-30 13:02:17 +0000

    pvr.tvlink: bump to 2.4.2

--- packages/mediacenter/kodi-binary-addons/pvr.tvlink/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.tvlink/package.mk
@@ -3,12 +3,12 @@
 # Copyright (C) 2011-present AlexELEC (https://alexelec.tv)
 
 PKG_NAME="pvr.tvlink"
-PKG_VERSION="2.4.1"
-PKG_SHA256="9d36249325140b47a9ba458fa5ba7afb0b432f2b20b04a4824f285b095a3ed2c"
-PKG_REV="1"
+PKG_VERSION="2.4.2"
+PKG_SHA256="478fd5df40ca73c139f00ef4e59a4aababd10c5cd080470ee81058ce6396bd5b"
+PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/AlexELEC/pvr.tvlink-20"
+PKG_SITE="https://github.com/AlexELEC/pvr.tvlink"
 PKG_URL="${PKG_SITE}/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain kodi-platform pugixml zlib xz"
 PKG_SECTION=""
